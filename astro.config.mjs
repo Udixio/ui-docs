2f6e2c80aa28e88ddb47cf2d4e4f35f22f67daaf
Joël <vigreux.joel@hotmail.com>
2023-10-06 16:54:07 +0200

    Update package-lock.json with new dependencies
    Added new dependencies to package-lock.json. These include `@astrojs/react`, `@types/react`, `@types/react-dom`, and several others. For each dependency, the version, resolved URL, integrity, and other relevant details have been set. This update is crucial to ensure the dependability and functionality of the software, as it relies on these libraries.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,28 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Udixio UI',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Getting started',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Installation', link: '/getting-started/installation/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'Udixio UI',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Getting started',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Installation',
+        link: '/getting-started/installation/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), react()]
 });
